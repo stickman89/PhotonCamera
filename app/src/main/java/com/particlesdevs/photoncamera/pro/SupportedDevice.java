c3b4dccac078390d0963d68c7fc3a4edcd8a2503
stickman89 <csstikman89@aol.com>
2025-12-10 16:36:54 +0000

    Look to this fork for specificsettings at SupportedDevice.java

--- app/src/main/java/com/particlesdevs/photoncamera/pro/SupportedDevice.java
+++ app/src/main/java/com/particlesdevs/photoncamera/pro/SupportedDevice.java
@@ -69,7 +69,7 @@ public class SupportedDevice {
     }
 
     private void loadSupportedDevicesList() throws IOException {
-        BufferedReader in = HttpLoader.readURL("https://raw.githubusercontent.com/eszdman/PhotonCamera/dev/app/SupportedList.txt",250);
+        BufferedReader in = HttpLoader.readURL("https://raw.githubusercontent.com/stickman89/PhotonCamera/dev/app/SupportedList.txt",250);
         String str;
         while ((str = in.readLine()) != null) {
             mSupportedDevicesSet.add(str);
